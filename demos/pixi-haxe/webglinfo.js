55e4c863ac636720ca32e59917ced427dbc7d807
Adi <adiinteractive@gmail.com>
2016-06-29 12:55:46 +0100

    updated sample

--- demos/pixi-haxe/webglinfo.js
+++ demos/pixi-haxe/webglinfo.js
@@ -1,7 +1,5 @@
 (function (console) { "use strict";
-Math.__name__ = true;
 var Reflect = function() { };
-Reflect.__name__ = true;
 Reflect.field = function(o,field) {
 	try {
 		return o[field];
@@ -9,89 +7,16 @@ Reflect.field = function(o,field) {
 		return null;
 	}
 };
-var Std = function() { };
-Std.__name__ = true;
-Std.string = function(s) {
-	return js_Boot.__string_rec(s,"");
-};
-var js_Boot = function() { };
-js_Boot.__name__ = true;
-js_Boot.__string_rec = function(o,s) {
-	if(o == null) return "null";
-	if(s.length >= 5) return "<...>";
-	var t = typeof(o);
-	if(t == "function" && (o.__name__ || o.__ename__)) t = "object";
-	switch(t) {
-	case "object":
-		if(o instanceof Array) {
-			if(o.__enum__) {
-				if(o.length == 2) return o[0];
-				var str2 = o[0] + "(";
-				s += "\t";
-				var _g1 = 2;
-				var _g = o.length;
-				while(_g1 < _g) {
-					var i1 = _g1++;
-					if(i1 != 2) str2 += "," + js_Boot.__string_rec(o[i1],s); else str2 += js_Boot.__string_rec(o[i1],s);
-				}
-				return str2 + ")";
-			}
-			var l = o.length;
-			var i;
-			var str1 = "[";
-			s += "\t";
-			var _g2 = 0;
-			while(_g2 < l) {
-				var i2 = _g2++;
-				str1 += (i2 > 0?",":"") + js_Boot.__string_rec(o[i2],s);
-			}
-			str1 += "]";
-			return str1;
-		}
-		var tostr;
-		try {
-			tostr = o.toString;
-		} catch( e ) {
-			return "???";
-		}
-		if(tostr != null && tostr != Object.toString && typeof(tostr) == "function") {
-			var s2 = o.toString();
-			if(s2 != "[object Object]") return s2;
-		}
-		var k = null;
-		var str = "{\n";
-		s += "\t";
-		var hasp = o.hasOwnProperty != null;
-		for( var k in o ) {
-		if(hasp && !o.hasOwnProperty(k)) {
-			continue;
-		}
-		if(k == "prototype" || k == "__class__" || k == "__super__" || k == "__interfaces__" || k == "__properties__") {
-			continue;
-		}
-		if(str.length != 2) str += ", \n";
-		str += s + k + " : " + js_Boot.__string_rec(o[k],s);
-		}
-		s = s.substring(1);
-		str += "\n" + s + "}";
-		return str;
-	case "function":
-		return "<function>";
-	case "string":
-		return o;
-	default:
-		return String(o);
-	}
-};
 var samples_webglinfo_Main = function() {
 	this.contextName = "";
-	this.log("WebGL Support (no context properties set): " + Std.string(this.detectWebGL()));
-	this.displayWebGLProperties();
+	var checkSupport = this.detectWebGL();
+	this.log("WebGL Support (no context properties set): " + (checkSupport == null?"null":"" + checkSupport));
+	if(checkSupport) this.displayWebGLProperties();
 	this.log("---------------------------------------------------");
-	this.log("WebGL Support: " + Std.string(this.detectWebGL({ failIfMajorPerformanceCaveat : true})));
-	this.displayWebGLProperties();
+	checkSupport = this.detectWebGL({ failIfMajorPerformanceCaveat : true});
+	this.log("WebGL Support (performance caveat set): " + (checkSupport == null?"null":"" + checkSupport));
+	if(checkSupport) this.displayWebGLProperties();
 };
-samples_webglinfo_Main.__name__ = true;
 samples_webglinfo_Main.main = function() {
 	new samples_webglinfo_Main();
 };
@@ -147,8 +72,6 @@ samples_webglinfo_Main.prototype = {
 		window.document.writeln(msg + "<br/>");
 	}
 };
-String.__name__ = true;
-Array.__name__ = true;
 samples_webglinfo_Main.main();
 })(typeof console != "undefined" ? console : {log:function(){}});
 
