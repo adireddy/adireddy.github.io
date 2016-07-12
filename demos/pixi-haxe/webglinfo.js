36f6b04b06868f7c21f427bb1a6874ff6fecde7d
Adi <adiinteractive@gmail.com>
2016-07-12 14:46:07 +0100

    updated samples

--- demos/pixi-haxe/webglinfo.js
+++ demos/pixi-haxe/webglinfo.js
@@ -67,7 +67,11 @@ samples_webglinfo_Main.prototype = {
 			this.log("Max Render Buffer Size",gl.getParameter(gl.MAX_RENDERBUFFER_SIZE));
 			this.log("Max Texture Size",gl.getParameter(gl.MAX_TEXTURE_SIZE));
 			this.log("Max Texture Image Units",gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS));
+			this.log("Max Vertex Texture Image Units",gl.getParameter(gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS));
+			this.log("Max Combined Texture Image Units",gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS));
 			this.log("Max Fragmented Uniform Vectors",gl.getParameter(gl.MAX_FRAGMENT_UNIFORM_VECTORS));
+			this.log("Max Vertex Attributes",gl.getParameter(gl.MAX_VERTEX_ATTRIBS));
+			this.log("Max Varying Vectors",gl.getParameter(gl.MAX_VARYING_VECTORS));
 			this.log("RGBA Bits","[" + gl.getParameter(gl.RED_BITS) + ", " + gl.getParameter(gl.GREEN_BITS) + ", " + gl.getParameter(gl.BLUE_BITS) + ", " + gl.getParameter(gl.ALPHA_BITS) + "]");
 			this.log("Depth & Stencil Bits","[" + gl.getParameter(gl.DEPTH_BITS) + ", " + gl.getParameter(gl.STENCIL_BITS) + "]");
 			if(gl.getExtension("WEBGL_draw_buffers") != null) this.log("Max Color Buffers",gl.getParameter(gl.getExtension("WEBGL_draw_buffers").MAX_DRAW_BUFFERS_WEBGL));
